6875aed18f1337d54659903f96c82ce0a2a8b0be
MF366-Coding <mateus366@vivaldi.net>
2023-03-06 19:07:48 +0000

    Still nothing for now (but new things will come...)

--- intro.py
+++ intro.py
@@ -1,34 +1,18 @@
 
 # The Psychologist
-
 # By MF366 and Norb
+# If you use this project, please identify us as the main project's creators.
+# Repo available at: https://github.com/MF366-Coding
 
 # To play, type the command that replies to the question or obaervation. 
 
-# If you use this project, please identify us as the main project's creators.
-
 # WARNING!
-
 # This game may contain disturbing stuff happening.
-
-# Repo available at: https://github.com/MF366-Coding
 
 # This project does not work without the other Chapter Files. 
 
 # Thanks for downloading this project.
 
 # We'll be adding more stuff so don't worry! 
-
-
-
-
-
-
-
-
-
-
-
-
 
 
