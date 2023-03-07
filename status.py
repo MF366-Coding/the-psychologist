b1c880dbae0804decb11de5c7e3b7dc839b4e469
MF366-Coding <mateus366@vivaldi.net>
2023-03-07 18:47:11 +0000

    Updating the new 'status' file/module

--- status.py
+++ status.py
@@ -3,31 +3,50 @@
 
 from clsCmd import clear
 
-class Names:
-    class Player:
-        name = "Peter"
-        surname = "Nest"
-        full = name+" "+surname
+class Status:
+    class Names:
+        class Player:
+            name = "Peter"
+            surname = "Nest"
+            full = name+" "+surname
 
-    class Mother:
-        name = "Linda"
-        surname = "Johnson"
-        full = name+" "+surname
-        
-    class StepFather:
-        name = "Gary"
-        surname = "Johnson"
-        full = name+" "+surname
+        class Mother:
+            name = "Linda"
+            surname = "Johnson"
+            full = name+" "+surname
+            
+        class StepFather:
+            name = "Gary"
+            surname = "Johnson"
+            full = name+" "+surname
 
-    class Father:
-        name = "Joseph"
-        surname = "Nest"
-        full = name+" "+surname
+        class Father:
+            name = "Joseph"
+            surname = "Nest"
+            full = name+" "+surname
+            
+        class Psychologist:
+            name = "Mary"
+            surname = "Nice"
+            full = name+" "+surname
+    
+    class Health:
+        meter = 150
+        lifeString = str(maxim)
+        full = str("Health: "+lifeString+" HP\n")
         
-    class Psychologist:
-        name = "Mary"
-        surname = "Nice"
-        full = name+" "+surname
+    class Rage:
+        meter = False
+        full = str("In rage? "+meter+"\n")
         
-def 
-
+    class Hunger:
+        meter = False
+        full = str("Hungry? "+meter+'\n')
+        
+    # Still gonna add much more :D
+        
+def statusEdit(amount, change):
+    if change == 1:
+        if amount == 0:
+            pass
+    
