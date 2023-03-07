377a632011e29350bd00e887765afdccaec18043
MF366 <109810178+MF366-Coding@users.noreply.github.com>
2023-03-07 13:51:42 +0000

    Finally something half useful
    Just half but still useful... 🙃

---
+++ status.py
@@ -0,0 +1,19 @@
+
+# The Status Module
+
+class Names:
+    class Player:
+        name = "Peter"
+        surname = "Nest"
+        full = name+" "+surname
+
+    class Mother:
+        name = "Linda"
+        surname = "Johnson"
+        full = name+" "+surname
+        
+    class StepFather:
+        name = "Gary"
+        surname = "Johnson"
+        full = name+" "+surname
+
